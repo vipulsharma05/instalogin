ed7cb94484f802578be0e2257305c51dc2218a67
VIPUL SHARMA <131189393+vipulsharma05@users.noreply.github.com>
2024-07-27 23:54:46 +0530

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,28 @@
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('/saveLogin', {  // Update with your actual backend URL
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            alert('Login information saved!');
+        } else {
+            alert('An error occurred!');
+        }
+    })
+    // .catch(error => {
+    //     console.error('Error:', error);
+    //     alert('An error occurred!');
+    // });
+});
+
+  
